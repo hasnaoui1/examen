0544839d69a6166972477a292f1a865ac7f0d5da
hasnaoui1 <108799947+hasnaoui1@users.noreply.github.com>
2025-12-18 19:13:21 +0100

    Fix typo in response message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('Hello World! testing github actionsxd2');
+    res.send('Hello World! testing github actionss');
 });
 app.listen(port, () => {
 
