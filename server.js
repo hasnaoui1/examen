41c7dc98e5f72ef17f2234998826018351477fc9
hasnaoui1 <108799947+hasnaoui1@users.noreply.github.com>
2025-12-18 16:59:37 +0100

    Update server.js again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('Hello World! testing github actions');
+    res.send('Hello World! testing github actions2');
 });
 app.listen(port, () => {
 
